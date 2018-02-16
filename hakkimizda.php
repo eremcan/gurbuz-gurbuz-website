58c0a112d767046faef12813ef8efc69641ff690
Çağatay Erem <cagatayerem@gmail.com>
2018-02-16 14:15:30 +0300

    anasayfa hakkimizda ekibimiz sayfaları girildi. php ye aktarıldı. Belli başlı düzenlemeler yapılacak.+1
    Signed-off-by: Çağatay Erem <cagatayerem@gmail.com>

---
+++ hakkimizda.php
@@ -0,0 +1,99 @@
+<?php include 'header.php'; ?>
+
+    <!-- Page Title -->
+    <div class="page-title">
+        <div class="container">
+            <div class="row">
+                <div class="span12">
+                    <i class="icon-user page-title-icon"></i>
+                    <h2>About Us /</h2>
+                    <p>Below you can find more information about our company</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- About Us Text -->
+    <div class="about-us container">
+        <div class="row">
+            <div class="about-us-text span12">
+                <h4>About Andia</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper <span class="violet">suscipit lobortis</span>
+                    nisl ut aliquip ex ea commodo consequat. Lorem ipsum <strong>dolor sit amet</strong>, consectetur
+                    adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Ut wisi enim ad minim veniam, quis
+                    nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem
+                    ipsum dolor sit amet, consectetur adipisicing elit, sed do <strong>eiusmod tempor</strong>
+                    incididunt.</p>
+                <h4>Our Mission</h4>
+                <p>Lorem ipsum dolor sit amet, <span class="violet">consectetur adipisicing</span> elit, sed do eiusmod
+                    tempor incididunt ut labore et. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+                    suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, <strong>consectetur
+                        adipisicing</strong> elit, sed do eiusmod <span class="violet">tempor incididunt</span> ut
+                    labore et. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis
+                    nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt.</p>
+                <h4>Why Choose Us</h4>
+                <p>Lorem ipsum dolor sit amet, <strong>consectetur adipisicing elit</strong>, sed do eiusmod tempor
+                    incididunt ut labore et. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+                    suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Ut wisi enim ad minim veniam, quis
+                    <span class="violet">nostrud exerci</span> tation ullamcorper suscipit lobortis nisl ut aliquip ex
+                    ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Meet Our Team -->
+
+
+    <!-- Testimonials -->
+    <div class="testimonials container">
+        <div class="testimonials-title">
+            <h3>Testimonials</h3>
+        </div>
+        <div class="row">
+            <div class="testimonial-list span12">
+                <div class="tabbable tabs-below">
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="A">
+                            <img src="assets/img/testimonials/1.jpg" title="" alt="">
+                            <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                                do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet,
+                                consectetur..."<br/><span class="violet">Lorem Ipsum, dolor.co.uk</span></p>
+                        </div>
+                        <div class="tab-pane" id="B">
+                            <img src="assets/img/testimonials/2.jpg" title="" alt="">
+                            <p>"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis
+                                nisl ut aliquip ex ea commodo consequat. Ut wisi enim ad minim veniam, quis nostrud
+                                exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                                consequat..."<br/><span class="violet">Minim Veniam, nostrud.com</span></p>
+                        </div>
+                        <div class="tab-pane" id="C">
+                            <img src="assets/img/testimonials/3.jpg" title="" alt="">
+                            <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                                do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet,
+                                consectetur..."<br/><span class="violet">Lorem Ipsum, dolor.co.uk</span></p>
+                        </div>
+                        <div class="tab-pane" id="D">
+                            <img src="assets/img/testimonials/1.jpg" title="" alt="">
+                            <p>"Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis
+                                nisl ut aliquip ex ea commodo consequat. Ut wisi enim ad minim veniam, quis nostrud
+                                exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                                consequat..."<br/><span class="violet">Minim Veniam, nostrud.com</span></p>
+                        </div>
+                    </div>
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#A" data-toggle="tab"></a></li>
+                        <li class=""><a href="#B" data-toggle="tab"></a></li>
+                        <li class=""><a href="#C" data-toggle="tab"></a></li>
+                        <li class=""><a href="#D" data-toggle="tab"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include 'footer.php'; ?>
