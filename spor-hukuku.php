e231b6fa06016cd2c2d9fe7e8962dd326ded8eb2
Çağatay Erem <cagatay.erem@partner.turktelekom.com.tr>
2018-08-11 00:44:13 +0300

    Yeni Hosting eklendi. Resimler Değiştirildi.

--- spor-hukuku.php
+++ spor-hukuku.php
@@ -1,16 +1,33 @@
 <?php include "header.php"; ?>
 
 <div class="">
-    <img src="assets/img/iphonedeneme.jpg" alt=" " class="img-width-full">
+    <img src="assets/img/index-category/spor-hukuku-ust.png" alt=" " class="img-width-full">
 </div>
 <div class="container margin-bottom-3p kategori-indextop">
     <div class="row">
         <div class="span12">
             <h3 style="color: #5d5d5d">SPOR HUKUKU</h3>
-           <p>Amerika’da 80’li, Avrupa’da ise 90’lı yıllarda gelişimini tamamlayan ve halen dünyada yükselen hukuk dalı olan <strong>Spor Hukuku</strong>’na dair meseleler mutaka yetkin bir hukukçu nezaretinde çözülmeye çalışılmalıdır. Çünkü diğer hukuk dalları ile karşılaştırıldığında <strong>Spor Hukuku</strong>’nun kendine has çok farklı dinamikleri ve ruhu vardır. </p>
-            <p><strong>Spor Hukuku</strong>’nun ülkemizdeki bu zamana kadarki uygulamaları göstermektedir ki, spor kulüpleri ve sporcular <strong>Spor Hukuku</strong>’na muktedir hukukçular ile çalışmazsa ve Spor Hukuku’nun kendine has doğası gözardı edilirse çok büyük hatalar kaçınılmaz olmakta ve sonucunda çok büyük maddi kayıplar ve manevi yıkımlar gerçekleşmektedir. Bir spor kulübünün bilinçsizce tarafı olduğu bir sözleşme hem kulübe menfi yönde maddi sonuçlar doğurmakta hem de bu kulübü desekleyen milyonlarca destekçiye manevi zararlar vermektedir.</p>
+            <p>Amerika’da 80’li, Avrupa’da ise 90’lı yıllarda gelişimini tamamlayan ve halen dünyada yükselen hukuk dalı
+                olan <strong>Spor Hukuku</strong>’na dair meseleler mutaka yetkin bir hukukçu nezaretinde çözülmeye
+                çalışılmalıdır. Çünkü diğer hukuk dalları ile karşılaştırıldığında <strong>Spor Hukuku</strong>’nun
+                kendine has çok farklı dinamikleri ve ruhu vardır. </p>
+            <div class="span6">
+                <img src="assets/img/index-category/spor-hukuku-cas-alt.png" alt=" " class="img-width-full-sporhukuk" style="padding-bottom: 4%">
+            </div>
+            <p><strong>Spor Hukuku</strong>’nun ülkemizdeki bu zamana kadarki uygulamaları göstermektedir ki, spor
+                kulüpleri ve sporcular <strong>Spor Hukuku</strong>’na muktedir hukukçular ile çalışmazsa ve Spor
+                Hukuku’nun kendine has doğası gözardı edilirse çok büyük hatalar kaçınılmaz olmakta ve sonucunda çok
+                büyük maddi kayıplar ve manevi yıkımlar gerçekleşmektedir. Bir spor kulübünün bilinçsizce tarafı olduğu
+                bir sözleşme hem kulübe menfi yönde maddi sonuçlar doğurmakta hem de bu kulübü desekleyen milyonlarca
+                destekçiye manevi zararlar vermektedir.</p>
 
-          <p>Sporcu ya da kulüpler bu zararları minimum seviyeye indirgemek için muhakkak işin ehli hukuçular ile çalışmalıdır. Hem ulusal hem de uluslararası Spor Hukuku mevzuatına hakim, Spor Hukuku’nun Amerika başta olmak üzere Avrupa ve diğer kıtalardaki uygulamalarını iyi muhakeme edebilen, yeni gelişmeleri sürekli takip eden, sözleşme konusunda sizleri en iyi koruma altına alabilecek ve çözüm odaklı sonuçlar üretebilecek yetkin hukuçularımız bir Sporcu ya da Kulübün Spor Hukuku ile ilintili her türlü sözleşmesini hazırlamak ve sizleri Sporcu ya da Kulüp olarak hem ulusal hem de uluslararası alanda sporun her türlü mecra ve yasal mercilerinde temsil etmek için hazırdır. </p>
+            <p>Sporcu ya da kulüpler bu zararları minimum seviyeye indirgemek için muhakkak işin ehli hukuçular ile
+                çalışmalıdır. Hem ulusal hem de uluslararası Spor Hukuku mevzuatına hakim, Spor Hukuku’nun Amerika başta
+                olmak üzere Avrupa ve diğer kıtalardaki uygulamalarını iyi muhakeme edebilen, yeni gelişmeleri sürekli
+                takip eden, sözleşme konusunda sizleri en iyi koruma altına alabilecek ve çözüm odaklı sonuçlar
+                üretebilecek yetkin hukuçularımız bir Sporcu ya da Kulübün Spor Hukuku ile ilintili her türlü
+                sözleşmesini hazırlamak ve sizleri Sporcu ya da Kulüp olarak hem ulusal hem de uluslararası alanda
+                sporun her türlü mecra ve yasal mercilerinde temsil etmek için hazırdır. </p>
 
             <p>Bu konularda danışmanlık hizmeti almak için randevu talep ediniz: <span class="violet">info@gurbuz-gurbuz.av.tr</span>
             </p>
